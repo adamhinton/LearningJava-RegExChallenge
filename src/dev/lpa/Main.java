80f383e8f39b06279fc4baddea17f9cc994c1568
adamd <adam.d.hinton@gmail.com>
2024-04-20 18:38:46 -0400

    challenge 1

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // [1] regex matching exactly "Hello, World!"
+            // use matches method on string to check if input matches pattern
+            // use only literal characters in regex
+
+        String hw = "Hello, World!";
+
+        System.out.println("[1] " + "Hello, World!".matches(hw));
+
+        // [2]
+
     }
 }
