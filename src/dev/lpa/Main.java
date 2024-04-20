a70e41528bf402ea2375bdb12e7c95dfd7cde76c
adamd <adam.d.hinton@gmail.com>
2024-04-20 18:43:52 -0400

    challenge 2

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -12,6 +12,15 @@ public class Main {
         System.out.println("[1] " + "Hello, World!".matches(hw));
 
         // [2]
+            // regex: starting with uppercase letter, followed by zero or more lowercase letters, ending with period
 
+        String ch2 = "[A-Z].*\\.";
+
+        System.out.println("Abdfas.".matches(ch2));
+        System.out.println("Ab.".matches(ch2));
+        System.out.println("A.".matches(ch2));
+        System.out.println("Abdfas".matches(ch2));
+        System.out.println("bdfas".matches(ch2));
+        System.out.println("bdfas".matches(ch2));
     }
 }
